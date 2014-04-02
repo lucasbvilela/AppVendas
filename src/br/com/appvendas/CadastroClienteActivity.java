ef84da47fc79755b781cf98b69b71961436b9f82
ramonsantos <ramonsantos.pe@gmail.com>
2014-04-02 17:26:49 -0300

    Criado uma Activity para tela de Cadastro de cliente

---
+++ src/br/com/appvendas/CadastroClienteActivity.java
@@ -0,0 +1,25 @@
+package br.com.appvendas;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class CadastroClienteActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cadastro_cliente);
+
+		
+	}
+
+	
+}
