e6cb6f63c2ee8b0e766c9c823aec047289c6e8b0
ramonsantos <ramonsantos.pe@gmail.com>
2014-04-04 00:12:32 -0300

    Primeira proposta para tela inicial concluída.

--- src/br/com/appvendas/TelaPrincipalActivity.java
+++ src/br/com/appvendas/TelaPrincipalActivity.java
@@ -1,7 +1,10 @@
 package br.com.appvendas;
 
+import br.com.appvendas.telas.Vendas;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TelaPrincipalActivity extends Activity {
 
@@ -12,4 +15,25 @@ public class TelaPrincipalActivity extends Activity {
 
 	}
 
+	//Eventos dos botoes da tela principal
+	public void bVendas(View v) {
+		//Botao de Vendas
+		startActivity(new Intent(this, Vendas.class));
+	}
+
+	public void bClientes(View v) {
+
+	}
+
+	public void bProdutos(View v) {
+
+	}
+
+	public void bRelatorios(View v) {
+
+	}
+
+	public void bCatalogo(View v) {
+
+	}
 }
